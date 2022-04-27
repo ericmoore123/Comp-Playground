b16033a6a78747e75f099c8919667589aa1aaf86
Eric Moore <51104403+ericmoore123@users.noreply.github.com>
2022-04-26 22:25:48 -0400

    Finished promises

--- 2.js
+++ 2.js
@@ -13,7 +13,7 @@
 // - have the function start the operation and return right away, so our program can still be responsive to other events
 // - be notified with the result of the operation, when it eventually completes.
 
-// Promises
+// ========== Promises ==========
 // Promises are the foundation of asynchronous programming in modern JavaScript. A promise is an object returned 
 // by an asynchronous function, which represents the current state of the operation. At the time the promise is 
 // returned to the caller, the operation often isn't finished, but the promise object provides methods to handle 
@@ -23,18 +23,35 @@ import fetch from 'node-fetch';
 const fetchPromise = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json');
 // Step 1
 console.log(fetchPromise); // This shows incomplete promise return object "Promise { <pending> }" in logs FIRST 
-
 // Step 3
 fetchPromise.then( res => {
     console.log("Response status: ", res.status); // This shows finished operation in logs THIRD
 });
-
 // Step 2
 console.log("Started request..."); // This shows fetch is being initiated in logs SECOND
 
 
-// Chaining Promises
+// ========== Chaining Promises ==========
 // MOST Minimal code approach
 fetchPromise // Make intiial async request from fetch call
     .then(res => res.json()) // convert response to json using .json() (promise is returned)
     .then(json => console.log(json[0].name)); // take retured json object and log first items name
+
+// ========== Handling multiple promises at once ==========
+const fetchPromise1 = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json');
+const fetchPromise2 = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/not-found');
+// const fetchPromise3 = fetch('bad-scheme://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json');
+
+Promise.all([fetchPromise1, fetchPromise2]) //Fetch all requests and return each requests response 
+  .then( responses => { // Pass responses object
+    // console.log(typeof responses);
+    for (const response of responses) { // Loop over response object and seperate logs for each request
+      console.log(`${response.url}: ${response.status}`); // Log response url and status code
+    }
+  })
+  .catch( error => { // Error handler if any initial fetch fails
+    console.error(`Failed to fetch: ${error}`)
+  });
+
+
+// ========== Async Await ==========
